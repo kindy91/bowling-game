49b7d6b1e245100adf6d7d09ffde1bcda8941ede
Abdullah Elkindy <abdullah.kindy@gmail.com>
2026-04-21 22:32:26 +0200

    feat: add game result

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle
+  ]
 })
 export class App {
   protected readonly title = signal('bowling-game');
